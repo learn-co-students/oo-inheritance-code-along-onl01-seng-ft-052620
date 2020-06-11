1f241c9cb2ffef18d6a540889ed6ac43fbfc5d36
shazy89 <erdo.shazy123@gmail.com>
2020-06-11 19:33:03 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,15 @@
+require "pry"
 require_relative "./vehicle.rb"
+
+
+
+class Car < Vehicle
+    def go
+      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+  end
+
+
+  Mercedes = Car.new("Mercedes", "Big")
+
+  binding.pry
